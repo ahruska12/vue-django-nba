0cff296bb7c0dfc5b1cc528c091ad2acd9a336f3
MattBooth1225 <111766064+MattBooth1225@users.noreply.github.com>
2023-04-12 10:11:05 -0500

    Created APIService.js

---
+++ nba-stattracker-vue/src/http/APIService.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+const API_URL ='fullstack2023.pythonanywhere.com'; /* 'http://localhost:8000' http://127.0.0.1:8000/ or  'http://127.0.0.1:8000'*/
+
+export class APIService {
+  constructor() {
+
+  }
+
+  getTeam(param_pk) {
+    const url = `${API_URL}/api/teams/${param_pk}`;
+    let jwtToken = localStorage.getItem('token');
+    console.log(":::jwtToken:::::"+jwtToken);
+    const headers = {Authorization: `jwt ${jwtToken}`};
+    return axios.get(url, {headers: {Authorization: `jwt ${jwtToken}`}});
+  }
+
+  getTeamList() {
+    const url = `${API_URL}/api/teams/`;
+    let jwtToken = localStorage.getItem('token');
+    console.log(":::jwtToken:::::"+jwtToken);
+    const headers = {Authorization: `jwt ${jwtToken}`};
+    return axios.get(url, {headers: headers});
+
+  }
+
+  authenticateLogin(credentials) {
+    const url = `${API_URL}/auth/`;
+    return axios.post(url, credentials);
+  }
+
+  registerUser(credentials) {
+     const url = `${API_URL}/register/`;
+     credentials.customusername = credentials.username
+     return axios.post(url, credentials);
+  }
+}
