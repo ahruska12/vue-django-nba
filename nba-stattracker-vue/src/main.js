869591ee4a0e5135e5edd2e314993750492fce26
MattBooth1225 <111766064+MattBooth1225@users.noreply.github.com>
2023-04-14 11:15:08 -0500

    Installed Vuex

--- nba-stattracker-vue/src/main.js
+++ nba-stattracker-vue/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import store from './store'
+import axios from 'axios'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store, axios).use(router).mount('#app')
