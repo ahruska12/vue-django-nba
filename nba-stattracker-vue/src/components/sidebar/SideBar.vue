681a7f6681c3742b8bca0b173b41f37636117078
Cameron Croghan <cameron.croghan@gmail.com>
2023-05-09 17:48:07 -0500

    sidebar continue

--- nba-stattracker-vue/src/components/sidebar/SideBar.vue
+++ nba-stattracker-vue/src/components/sidebar/SideBar.vue
@@ -15,13 +15,13 @@ export default {
   <div class="sidebar" :style="{ width: sidebarWidth }">
     <h1>
       <span v-if="collapsed">
-        <div>V</div>
-        <div>S</div>
+        <div>NBA</div>
+        <div>App</div>
       </span>
-      <span v-else>NBA StatTracker</span>
+      <span v-else>NBA Stat </span>
     </h1>
 
-                  <router-link class="btn-primary" to="/">Home Page</router-link>
+                  <SidebarLink icon="fas fa-home" to="/">Home Page</SidebarLink>
                   <router-link class="btn-secondary" :to="{ name: 'Team', params: { id: 'compare' } }">Team Comparison</router-link>
                   <router-link class="btn-secondary" :to="{ name: 'Player', params: { id: 'compare' } }">Player Comparison</router-link>
                   <router-link class="btn-secondary" :to="{name:'ListPlayers'}">Player List</router-link>
@@ -31,7 +31,6 @@ export default {
                   <router-link class="btn-secondary" v-if="authenticated" @click="favorite" :to="{name: 'TeamFavorites'}">Favorites</router-link>
                   <router-link class="btn-secondary" v-if="authenticated" @click="logout" :to="{name: 'AuthUser'}">Logout</router-link>
 
-      <SidebarLink to="/image" icon="fas fa-image">Icon Here</SidebarLink>
 
     <span
       class="collapse-icon"
@@ -45,9 +44,11 @@ export default {
 
 <style>
 :root {
-  --sidebar-bg-color: #2f855a;
-  --sidebar-item-hover: #38a169;
-  --sidebar-item-active: #276749;
+    --sidebar-bg-color: #2f855a;
+    --sidebar-item-hover: #38a169;
+    --sidebar-item-active: #276749;
+    --button-color: black;
+    --button-background-color: rgb(255, 202, 0);
 }
 </style>
 
@@ -88,4 +89,26 @@ export default {
   transform: rotate(180deg);
   transition: 0.2s linear;
 }
+
+.btn-primary {
+  color: var(--button-color);
+  background-color: var(--button-background-color);
+  border-radius: var(--border-radius);
+}
+
+.btn-primary:hover {
+  box-shadow: inset 0 0 0 20rem var(--darken-1);
+}
+
+.btn-primary:active {
+  box-shadow: inset 0 0 0 20rem var(--darken-2),
+    inset 0 3px 4px 0 var(--darken-3),
+    0 0 1px var(--darken-2);
+}
+
+.btn-primary:disabled,
+.btn-primary.is-disabled {
+  opacity: .5;
+}
+
 </style>
